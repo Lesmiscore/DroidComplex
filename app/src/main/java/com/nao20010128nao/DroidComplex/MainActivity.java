6a6277d3e518fc031ef0875228e090cd45406b7d
nao20010128nao <nao20010128@gmail.com>
2017-10-22 23:37:51 +0900

    追加

--- app/src/main/java/com/nao20010128nao/DroidComplex/MainActivity.java
+++ app/src/main/java/com/nao20010128nao/DroidComplex/MainActivity.java
@@ -3,11 +3,14 @@ package com.nao20010128nao.DroidComplex;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.astuetz.*;
 
+public class MainActivity extends AppCompatActivity {
+    PagerSlidingTabStrip tabs;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
 }
